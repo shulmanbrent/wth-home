79a2ace730f7c07b5d74b1d69e21873ec63222f1
Raghav Joshi <snowboarderwv@hnt-upper042.apn.wireless-pennnet.upenn.edu>
2014-11-14 00:04:25 -0500

    round 2 bitches

--- wthome/urls.py
+++ wthome/urls.py
@@ -6,9 +6,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wthome.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('home.urls')
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('home.urls'))
 )
 
 
