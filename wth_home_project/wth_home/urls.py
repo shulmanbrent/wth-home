4ed9cbc15b8344a3a3bc3159a8bbbc453edcc49f
Raghav Joshi <snowboarderwv@hnt-upper042.apn.wireless-pennnet.upenn.edu>
2014-11-13 23:41:58 -0500

    #fatal

--- wth_home_project/wth_home/urls.py
+++ wth_home_project/wth_home/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'wthome.views.home', name='home'),
+    # url(r'^$', 'wth_home.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    urlpatterns = patterns('',
-    url('^$', TemplateView.as_view(template_name='index.html')),
-    )
 )
-
-
-
