fda46182df88ad29a914fc6cdd136388ed69d7ad
davidwan <david_wan@rocketmail.com>
2015-03-26 22:58:32 -0400

    forgot pics + cleaning up

--- wthome/static/js/shortcodes.js
+++ wthome/static/js/shortcodes.js
@@ -1,19 +1,20 @@
-jQuery(document).ready(function($){
+function rnr_shortcodes() {
 	
+		
 	/* ------------------------------------------------------------------------ */
 	/* Accordion */
 	/* ------------------------------------------------------------------------ */
 	
-	$('.accordion').each(function(){
-	    var acc = $(this).attr("rel") * 2;
-	    $(this).find('.accordion-inner:nth-child(' + acc + ')').show();
-	     $(this).find('.accordion-inner:nth-child(' + acc + ')').prev().addClass("active");
+	jQuery('.accordion').each(function(){
+	    var acc = jQuery(this).attr("rel") * 2;
+	    jQuery(this).find('.accordion-inner:nth-child(' + acc + ')').show();
+	     jQuery(this).find('.accordion-inner:nth-child(' + acc + ')').prev().addClass("active");
 	});
 	
-	$('.accordion .accordion-title').click(function() {
-	    if($(this).next().is(':hidden')) {
-	        $(this).parent().find('.accordion-title').removeClass('active').next().slideUp(200);
-	        $(this).toggleClass('active').next().slideDown(200);
+	jQuery('.accordion .accordion-title').click(function() {
+	    if(jQuery(this).next().is(':hidden')) {
+	        jQuery(this).parent().find('.accordion-title').removeClass('active').next().slideUp(200);
+	        jQuery(this).toggleClass('active').next().slideDown(200);
 	    }
 	    return false;
 	});
@@ -22,8 +23,8 @@ jQuery(document).ready(function($){
 	/* Alert Messages */
 	/* ------------------------------------------------------------------------ */
 	
-	$(".alert-message .close").click(function(){
-		$(this).parent().animate({'opacity' : '0'}, 300).slideUp(300);
+	jQuery(".alert-message .close").live('click',function(){
+		jQuery(this).parent().animate({'opacity' : '0'}, 300).slideUp(300);
 		return false;
 	});
 	
@@ -31,52 +32,53 @@ jQuery(document).ready(function($){
 	/* Skillbar */
 	/* ------------------------------------------------------------------------ */
 	
-	$('.skillbar').each(function(){
-	    dataperc = $(this).attr('data-perc'),
-	    $(this).find('.skill-percentage').animate({ "width" : dataperc + "%"}, dataperc*10);
+	jQuery('.skillbar').each(function(){
+	    dataperc = jQuery(this).attr('data-perc'),
+	    jQuery(this).find('.skill-percentage').animate({ "width" : dataperc + "%"}, dataperc*10);
 	});
 	
 	/* ------------------------------------------------------------------------ */
 	/* Tabs */
 	/* ------------------------------------------------------------------------ */
 	
-	$('div.tabset').tabset();
+	jQuery('div.tabset').tabset();
 	
 	/* ------------------------------------------------------------------------ */
 	/* Toggle */
 	/* ------------------------------------------------------------------------ */
 	
-	if( $(".toggle .toggle-title").hasClass('active') ){
-		$(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
+	if( jQuery(".toggle .toggle-title").hasClass('active') ){
+		jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
 	}
 	
-	$(".toggle .toggle-title").click(function(){
-		if( $(this).hasClass('active') ){
-			$(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
+	jQuery(".toggle .toggle-title").click(function(){
+		if( jQuery(this).hasClass('active') ){
+			jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
 		}
 		else{
-			$(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
+			jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
 		}
 	});
 
 /* EOF document.ready */
-});
+
+};
 
 /* Tabset Function ---------------------------------- */
-(function ($) {
-$.fn.tabset = function () {
-    var $tabsets = $(this);
-    $tabsets.each(function (i) {
-        var $tabs = $('li.tab a', this);
-        $tabs.click(function (e) {
-            var $this = $(this);
-                panels = $.map($tabs, function (val, i) {
-                    return $(val).attr('href');
+(function (jQuery) {
+jQuery.fn.tabset = function () {
+    var jQuerytabsets = jQuery(this);
+    jQuerytabsets.each(function (i) {
+        var jQuerytabs = jQuery('li.tab a', this);
+        jQuerytabs.click(function (e) {
+            var jQuerythis = jQuery(this);
+                panels = jQuery.map(jQuerytabs, function (val, i) {
+                    return jQuery(val).attr('href');
                 });
-            $(panels.join(',')).hide();
-            $tabs.removeClass('selected');
-            $this.addClass('selected').blur();
-            $($this.attr('href')).show();
+            jQuery(panels.join(',')).hide();
+            jQuerytabs.removeClass('selected');
+            jQuerythis.addClass('selected').blur();
+            jQuery(jQuerythis.attr('href')).show();
             e.preventDefault();
             return false;
         }).first().triggerHandler('click');
